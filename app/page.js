128009ac66cfacd0e5f983f09f9c1ad8a0aea0e2
marian.estrada <ian_an@yahoo.com>
2024-09-23 16:17:52 -0600

    Commit for Week 3 assignment
    updated layout to change title
    updated page to add links for each assignment week
    added week-3 folder and files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-
+import Link from "next/link";
 
 export default function Home() {
+
+  let linkStyles = "underline text-cyan-600 hover:bg-cyan-300";
+
   return (
     <main>
-
+      <h1>Home</h1>
+          <Link className={linkStyles} href="./week-2/">Week 2 - New Next JS Project</Link><br></br>
+          <Link className={linkStyles} href="./week-3/">Week 3 - Objects & Props</Link><br></br>
     </main>
   );
 }
